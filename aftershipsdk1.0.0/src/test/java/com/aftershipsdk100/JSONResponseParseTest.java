9999d5dabf4cea582e777aed6d567114791fc799
Haroon <haroon91@gmail.com>
2016-05-10 21:18:54 +0800

    [Update] javadoc removed

---
+++ aftershipsdk1.0.0/src/test/java/com/aftershipsdk100/JSONResponseParseTest.java
@@ -0,0 +1,31 @@
+package com.aftershipsdk100;
+
+import com.aftershipsdk100.api.api.ApiDataManager;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class JSONResponseParseTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    ApiDataManager apiDataManager = ApiDataManager.getInstance();
+
+    @Test
+    public void parseTrackingTest_Null () {
+        JSONObject jsonObject = null;
+
+        
+
+
+
+
+    }
+}
