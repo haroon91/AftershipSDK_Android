2db2e934a971356e7200a78f8b27d9129c57e291
Haroon <haroon91@gmail.com>
2016-05-12 13:09:59 +0800

    [Update] Code refactor

--- aftershipsdk1.0.0/src/test/java/com/aftershipsdk100/JSONResponseParseTest.java
+++ aftershipsdk1.0.0/src/test/java/com/aftershipsdk100/JSONResponseParseTest.java
@@ -1,31 +1,9 @@
 package com.aftershipsdk100;
 
-import com.aftershipsdk100.sdk.api.ApiDataManager;
-
-import org.json.JSONObject;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class JSONResponseParseTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-
-    ApiDataManager apiDataManager = ApiDataManager.getInstance();
-
-    @Test
-    public void parseTrackingTest_Null () {
-        JSONObject jsonObject = null;
-
-        
 
 
-
-
-    }
 }
